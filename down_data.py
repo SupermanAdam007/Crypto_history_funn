4e2dff4b1a44bd34d08fe50e143ffef643d2f56b
Pavlát Adam <adam.pavlat@avast.com>
2017-09-06 18:10:55 +0200

    window mean added

--- down_data.py
+++ down_data.py
@@ -27,7 +27,7 @@ class Downloader(object):
 		res = requests.get(req)
 		return res
 
-	def get_histo(self, t_horiz='minute', fsym, tsym, limit=60, aggregate=1, e=''):
+	def get_histo(self, fsym, tsym, limit=60, aggregate=1, e='', t_horiz='minute'):
 		"""
 		t_horiz: minute, hour, day
 		fsyms: coin from: ex. 'XMR'
@@ -44,84 +44,14 @@ class Downloader(object):
 			e = self.DEFAULT_MARKET
 
 		req = '&'.join([
-			self.url + 'histo'+type+'?', 
+			self.url + 'histo'+t_horiz+'?', 
 			'fsym=' + fsym, 
 			'tsym=' + tsym, 
 			'limit=' + str(limit), 
 			'aggregate=' + str(aggregate), 
 			'e=' + e])
 
-		print('get_histominute request: ' + req)
-		res = requests.get(req)
-		return res
-
-
-	def get_histominute(self, fsym, tsym, limit=60, aggregate=1, e=''):
-		"""
-		fsyms: coin from: ex. 'XMR'
-		tsyms: coin to: ex. 'BTC'
-		e: markets, ex. 'Poloniex' ... default
-		aggregate: min = 1
-		limit: min = 1, max = 2000
-		"""
-		if e == '':
-			e = self.DEFAULT_MARKET
-
-		req = '&'.join([
-			self.url + 'histominute?', 
-			'fsym=' + fsym, 
-			'tsym=' + tsym, 
-			'limit=' + str(limit), 
-			'aggregate=' + str(aggregate), 
-			'e=' + e])
-
-		print('get_histominute request: ' + req)
-		res = requests.get(req)
-		return res
-
-	def get_histohour(self, fsym, tsym, limit=60, aggregate=1, e=''):
-		"""
-		fsyms: coin from: ex. 'XMR'
-		tsyms: coin to: ex. 'BTC'
-		e: markets, ex. 'Poloniex' ... default
-		aggregate: min = 1
-		limit: min = 1, max = 2000
-		"""
-		if e == '':
-			e = self.DEFAULT_MARKET
-
-		req = '&'.join([
-			self.url + 'histohour?', 
-			'fsym=' + fsym, 
-			'tsym=' + tsym, 
-			'limit=' + str(limit), 
-			'aggregate=' + str(aggregate), 
-			'e=' + e])
-
-		print('get_histohour request: ' + req)
-		res = requests.get(req)
-		return res
-
-	def get_histoday(self, fsym, tsym, limit=60, aggregate=1, e=''):
-		"""
-		fsyms: coin from: ex. 'XMR'
-		tsyms: coin to: ex. 'BTC'
-		e: markets, ex. 'Poloniex' ... default
-		aggregate: min = 1
-		limit: min = 1, max = 2000
-		"""
-		if e == '':
-			e = self.DEFAULT_MARKET
-
-		req = '&'.join([
-			self.url + 'histoday?', 
-			'fsym=' + fsym, 
-			'tsym=' + tsym, 
-			'limit=' + str(limit), 
-			'aggregate=' + str(aggregate), 
-			'e=' + e])
-
-		print('get_histohour request: ' + req)
+		print('get_histo' + t_horiz + ' request: ' + req)
 		res = requests.get(req)
 		return res
 
@@ -129,11 +59,5 @@ class Downloader(object):
 	def get_pricemulti_json(self, fsyms, tsyms, e=''):
 		return self.get_pricemulti(fsyms, tsyms, e).json()
 
-	def get_histominute_json(self, fsym, tsym, limit=60, aggregate=1, e=''):
-		return self.get_histominute(fsym, tsym, limit, aggregate, e='').json()
-
-	def get_histohour_json(self, fsym, tsym, limit=60, aggregate=1, e=''):
-		return self.get_histohour(fsym, tsym, limit, aggregate, e='').json()
-
-	def get_histoday_json(self, fsym, tsym, limit=60, aggregate=1, e=''):
-		return self.get_histoday(fsym, tsym, limit, aggregate, e='').json()
+	def get_histo_json(self, fsym, tsym, limit=60, aggregate=1, e='', t_horiz='minute'):
+		return self.get_histo(fsym, tsym, limit, aggregate, e, t_horiz).json()
