1cc2ea6ec3414046b18a0e98c73dcbae16f69fd3
Adam <adam.pavlat@gmail.com>
2017-09-06 07:41:11 +0200

    days added

--- down_data.py
+++ down_data.py
@@ -5,7 +5,7 @@ class Downloader(object):
 
 	def __init__(self):
 		self.url = 'https://min-api.cryptocompare.com/data/'
-		self.market = 'Poloniex'
+		self.DEFAULT_MARKET = 'Poloniex'
 
 	def get_pricemulti(self, fsyms, tsyms, e=''):
 		"""
@@ -15,7 +15,7 @@ class Downloader(object):
 		response_type=''
 		"""
 		if e == '':
-			e = self.market
+			e = self.DEFAULT_MARKET
 
 		req = '&'.join([
 			self.url + 'pricemulti?', 
@@ -27,11 +27,34 @@ class Downloader(object):
 		res = requests.get(req)
 		return res
 
-	def get_pricemulti_json(self, fsyms, tsyms, e=''):
-		return self.get_pricemulti(fsyms, tsyms, e).json()
+	def get_histo(self, t_horiz='minute', fsym, tsym, limit=60, aggregate=1, e=''):
+		"""
+		t_horiz: minute, hour, day
+		fsyms: coin from: ex. 'XMR'
+		tsyms: coin to: ex. 'BTC'
+		e: markets, ex. 'Poloniex' ... default
+		aggregate: min = 1
+		limit: min = 1, max = 2000
+		"""
+		types = ['minute', 'hour', 'day']
+		if t_horiz not in types:
+			raise Exception('Invalid t_horiz parameter... Choose from: ' + ','.join(types))
 
-	def get_pricemulti_json(self, fsyms, tsyms, e=''):
-		return self.get_pricemulti(fsyms, tsyms, e).json()
+		if e == '':
+			e = self.DEFAULT_MARKET
+
+		req = '&'.join([
+			self.url + 'histo'+type+'?', 
+			'fsym=' + fsym, 
+			'tsym=' + tsym, 
+			'limit=' + str(limit), 
+			'aggregate=' + str(aggregate), 
+			'e=' + e])
+
+		print('get_histominute request: ' + req)
+		res = requests.get(req)
+		return res
+
 
 	def get_histominute(self, fsym, tsym, limit=60, aggregate=1, e=''):
 		"""
@@ -42,7 +65,7 @@ class Downloader(object):
 		limit: min = 1, max = 2000
 		"""
 		if e == '':
-			e = self.market
+			e = self.DEFAULT_MARKET
 
 		req = '&'.join([
 			self.url + 'histominute?', 
@@ -56,6 +79,61 @@ class Downloader(object):
 		res = requests.get(req)
 		return res
 
+	def get_histohour(self, fsym, tsym, limit=60, aggregate=1, e=''):
+		"""
+		fsyms: coin from: ex. 'XMR'
+		tsyms: coin to: ex. 'BTC'
+		e: markets, ex. 'Poloniex' ... default
+		aggregate: min = 1
+		limit: min = 1, max = 2000
+		"""
+		if e == '':
+			e = self.DEFAULT_MARKET
+
+		req = '&'.join([
+			self.url + 'histohour?', 
+			'fsym=' + fsym, 
+			'tsym=' + tsym, 
+			'limit=' + str(limit), 
+			'aggregate=' + str(aggregate), 
+			'e=' + e])
+
+		print('get_histohour request: ' + req)
+		res = requests.get(req)
+		return res
+
+	def get_histoday(self, fsym, tsym, limit=60, aggregate=1, e=''):
+		"""
+		fsyms: coin from: ex. 'XMR'
+		tsyms: coin to: ex. 'BTC'
+		e: markets, ex. 'Poloniex' ... default
+		aggregate: min = 1
+		limit: min = 1, max = 2000
+		"""
+		if e == '':
+			e = self.DEFAULT_MARKET
+
+		req = '&'.join([
+			self.url + 'histoday?', 
+			'fsym=' + fsym, 
+			'tsym=' + tsym, 
+			'limit=' + str(limit), 
+			'aggregate=' + str(aggregate), 
+			'e=' + e])
+
+		print('get_histohour request: ' + req)
+		res = requests.get(req)
+		return res
+
+
+	def get_pricemulti_json(self, fsyms, tsyms, e=''):
+		return self.get_pricemulti(fsyms, tsyms, e).json()
+
 	def get_histominute_json(self, fsym, tsym, limit=60, aggregate=1, e=''):
 		return self.get_histominute(fsym, tsym, limit, aggregate, e='').json()
 
+	def get_histohour_json(self, fsym, tsym, limit=60, aggregate=1, e=''):
+		return self.get_histohour(fsym, tsym, limit, aggregate, e='').json()
+
+	def get_histoday_json(self, fsym, tsym, limit=60, aggregate=1, e=''):
+		return self.get_histoday(fsym, tsym, limit, aggregate, e='').json()
