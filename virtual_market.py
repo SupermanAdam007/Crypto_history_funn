872c24a4cf6589b9e5bc7485874eed3425c4f0a7
Adam <adam.pavlat@gmail.com>
2017-09-18 07:30:04 +0200

    todo

--- virtual_market.py
+++ virtual_market.py
@@ -1,68 +1,77 @@
-
-
 class Market(object):
 
 	
-	def __init__(self, btc_init, bcn_init, commision=0.0015, max_opens=10):
-		self.have_btc = btc_init
-		self.have_bcn = bcn_init
+	def __init__(self, commision=0.0015, max_opens=10):
 		self.commision = commision
 		self.buys_sells = 0
 		self.max_opens = max_opens
+		self.coins_list_btc = ['SYS','BCN','DGB','LBC','NOTE','PASC','NAUT','SC','NEOS','ETH','ZEC','MAID','XRP',
+			'BTS','XEM','SBD','STRAT','LTC','AMP','BCY','ETC','ARDR','POT','NXT','BELA','XBC',
+			'BTCD','XMR','NXC','XCP','DCR','BLK','RIC','STEEM','NMC','PINK','OMNI','SJCX','NAV']
+		self.coins_list_btc.append('BTC')
+		
+		self.status = dict()
+		self.init_status()
 
+	def init_status(self):
+		for coin_btc in self.coins_list_btc:
+    		self.status[coin_btc] = 0
 
-	def buy_bcn(self, amount, price_btc, verbose=False):
+    def manual_add_to_status(self, coin_name, amount):
+    	self.status[coin_name] = amount
+
+	def buy_bcn(self, coin_name, amount, price_btc, verbose=False):
 		if self.buys_sells == self.max_opens:
 			if verbose:
 				print('buys_sells == '+str(self.max_opens)+':', self.buys_sells == self.max_opens)
-			return ''
+			return False
 
-		if amount*price_btc*(1+self.commision) > self.have_btc:
+		if amount*price_btc*(1+self.commision) > self.status['BTC']:
 			if verbose:
 				print('Don\'t have enough BTC')
-				print('\tHave:', self.have_btc)
+				print('\tHave:', self.status['BTC'])
 				print('\tNeed:', amount*price_btc)
-			return ''
+			return False
 
-		self.have_bcn += amount - amount*self.commision
-		self.have_btc -= amount*price_btc
+		self.status[coin_name] += amount - amount*self.commision
+		self.status['BTC'] -= amount*price_btc
 
 		self.buys_sells += 1
 
 		if verbose:
-			print('# Buy BCN: amount =', amount, 
+			print('# Buy '+coin_name+': amount =', amount, 
 				'### price_btc =', price_btc)
 			self.print_have()
 
-		return 'a'
+		return True
 
 
-	def sell_bcn(self, amount, price_btc, verbose=False):
+	def sell_bcn(self, coin_name, amount, price_btc, verbose=False):
 		if self.buys_sells == -self.max_opens:
 			if verbose:
 				print('buys_sells == -'+str(self.max_opens)+':', self.buys_sells == -self.max_opens)
-			return ''
+			return False
 
-		if self.have_bcn < amount*(1+self.commision):
+		if self.status[coin_name] < amount*(1+self.commision):
 			if verbose:
 				print('Don\'t have enough BNC')
-			return ''
+			return False
 
-		self.have_bcn -= amount 
-		self.have_btc += amount*price_btc - amount*self.commision*price_btc
+		self.status[coin_name] -= amount 
+		self.status['BTC'] += amount*price_btc - amount*self.commision*price_btc
 
 		self.buys_sells -= 1
 		
 		if verbose:
-			print('# Sell BCN: amount =', amount, 
+			print('# Sell '+coin_name+': amount =', amount, 
 				'### price_btc =', price_btc)
 			self.print_have()
 
-		return 'a'
+		return True
 
 
 	def print_have(self):
-		print('have_bcn =', self.have_bcn)
-		print('have_btc =', self.have_btc, 
-			'[aprox. ', self.have_btc*4000, '$ pseudo]')
-		print('Total have aprox.:', self.have_bcn*0.0000004*4000 + self.have_btc*4000, '$ pseudo')
+		print('status[coin_name] =', self.status[coin_name])
+		print('status[\'BTC\'] =', self.status['BTC'], 
+			'[aprox. ', self.status['BTC']*4000, '$ pseudo]')
+		#print('[Total have aprox.:', self.status[coin_name]*0.0000004*4000 + self.status['BTC']*4000, '$ pseudo]')
